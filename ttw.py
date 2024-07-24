4b122a43135f9c2b0cc22b504978078778248c9e
vearahmad <69453119+vearahmad@users.noreply.github.com>
2024-07-24 16:24:29 -0400

    Update ttw.py

--- ttw.py
+++ ttw.py
@@ -40,6 +40,9 @@ b.set_handle_referer(True)
 b.set_handle_robots(False)
 b._factory.is_html = True
 
+b.addheaders = [('User-agent',
+                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/45.0.2454101'
+                 )]
 headers = [('authority': 'x.com',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9,ar-IQ;q=0.8,ar;q=0.7,es-US;q=0.6,es;q=0.5',
